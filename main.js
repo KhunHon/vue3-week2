aa357fa207bfe7f4411f74d664176124f03f943e
KhunHon <thehonkungleve0522@gmail.com>
2022-10-27 02:43:08 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product:"iPhone 14",
+            image:'./assets/images/iphone-12-white.png',
+            description:'Apple A14 Bionic',
+            url:'https://www.apple.com',
+            inStock:true,
+            inventory:0,
+            details: ["64GB","128GB","256GB"],
+            varians: [
+                {
+                    id: 239, color: "Purple", image:"./assets/images/iphone-12-purple.png",
+                    id: 240, color: "Red", image:"./assets/images/iphone-12-red.png",
+                    id: 241, color: "White", image:"./assets/images/iphone-12-white.png",
+                }
+            ]
+        }
+    }
+}).mount("#app")
